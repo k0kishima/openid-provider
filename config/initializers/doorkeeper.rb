e89284dbe69ec40fb874e863ae56acda9a8ea79c
Johndoe <56298669+togane@users.noreply.github.com>
2021-10-28 19:19:50 +0900

    fix doorkeeper configuration

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -14,7 +14,16 @@ Doorkeeper.configure do
     current_user || warden.authenticate!(scope: :user)
   end
 
+  access_grant_class 'Auth::AccessGrant'
   application_class 'Auth::RelyingParty'
 
   default_scopes  :openid
+
+  skip_authorization do
+    # NOTE:
+    # ここでのauthorization:
+    # 認可リクエストの際に認可内容をユーザーに確認する画面（「Authorization required」みたいなタイトル）
+    # 現状、外部のアプリケーションを登録するような仕様ではないため身内のことは信頼してスキップ
+    true
+  end
 end
